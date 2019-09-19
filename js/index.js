93c5b7937a924e8c286749d60d06e4d444b23c0a
shobhitmalarya8 <shobhitmalarya8@gmail.com>
2019-09-20 01:10:56 +0530

    links

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-});
-
-$(document).ready(function(){
+    $('.carousel').carousel();
     $('.collapsible').collapsible();
-})
 
-$(document).ready(function(){
-    $('.carousel').carousel();});
+    $('#send_mail').on("click",function () {
+        console.log("initiated");
+        link_will_be = "mailto:shobhitmalarya8@gmail.com?Subject=Hello%from%website;";
+        msg_body = $('#msg').val();
+        console.log(msg_body);
+        link_will_be = link_will_be+"body="+msg_body.replace(" ","%");
+        $('#actual_send_mail').attr("href", link_will_be);
+        console.log(link_will_be);
+        $('#actual_send_mail').click();
+        console.log($('#actual_send_mail').attr('href'));
+    });
+
+});
