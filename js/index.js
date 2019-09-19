66d1bc404ae58032885bf358c6bae380a7a987d7
shobhitmalarya8 <shobhitmalarya8@gmail.com>
2019-09-20 01:16:19 +0530

    mail fix

--- js/index.js
+++ js/index.js
@@ -7,12 +7,9 @@ $(document).ready(function(){
         console.log("initiated");
         link_will_be = "mailto:shobhitmalarya8@gmail.com?Subject=Hello%from%website;";
         msg_body = $('#msg').val();
-        console.log(msg_body);
         link_will_be = link_will_be+"body="+msg_body.replace(" ","%");
-        $('#actual_send_mail').attr("href", link_will_be);
-        console.log(link_will_be);
+        $('#contact_form').attr("action", link_will_be);
         $('#actual_send_mail').click();
-        console.log($('#actual_send_mail').attr('href'));
     });
 
 });
