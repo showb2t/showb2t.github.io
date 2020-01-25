63452959dc06b3f8747839d253c4cb5b711a4090
shobhitmalarya8 <shobhitmalarya8@gmail.com>
2020-01-25 13:43:01 +0530

    turtlr gif

--- res/bdmvn.js
+++
@@ -1,7 +0,0 @@
-    var animation = bodymovin.loadAnimation({
-    container: document.getElementById('loader'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'turtle.json'
-})
